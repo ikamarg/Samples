3e4d1cae76d4769bc74f3df202298007755103d4
Irakli <ikamarg14@gmail.com>
2021-10-25 20:56:25 +0400

    initial

---
+++ common/common.go
@@ -0,0 +1,5 @@
+package common
+
+func Sum(x,y int)int{
+  return x + y  
+}
---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+  "fmt"
+  "Samples/common"
+)
+
+func main(){
+
+  fmt.Println(common.Sum(1,2))
+  
+}
