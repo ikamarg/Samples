74a558e089d631d6eef7068824c977c62a9ff348
Irakli <ikamarg14@gmail.com>
2021-11-06 23:40:16 +0400

    cleared

--- main.go
+++ main.go
@@ -1,32 +1,5 @@
 package main
 
-import (
-	"Samples/irlog"
-	"fmt"
-	"sync"
-	"time"
-)
-
-func lg(logger *irlog.LogData, w string, wg *sync.WaitGroup) {
-	defer wg.Done()
-	for k := 0; k <= 10000; k++ {
-		logger.Log(1, w)
-	}
-
-}
-
 func main() {
-	logger, err := irlog.InitializeLogger("/home/irakli/go/src/Samples/Logs/log.txt", "/home/irakli/go/src/Samples/LogArchive/", 2, 1000)
-	if err != nil {
-		fmt.Println("errror")
-	}
-	wg := sync.WaitGroup{}
-	//just the two of us
-	wg.Add(1)
-	go lg(logger, "1", &wg)
-	time.Sleep(2 * time.Second)
-	wg.Add(1)
-	go lg(logger, "2", &wg)
-	wg.Wait()
-	// go lg(logger, "2")
+	return
 }
