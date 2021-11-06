f2ca0475f95663495d98cf6efd4dc9b8be9a7596
Irakli <ikamarg14@gmail.com>
2021-11-06 23:02:17 +0400

    add mutex

--- main.go
+++ main.go
@@ -1,12 +1,31 @@
 package main
 
 import (
-  "fmt"
-  "Samples/common"
+	"Samples/irlog"
+	"fmt"
+	"sync"
+	"time"
 )
 
-func main(){
+func lg(logger *irlog.LogData, w string, wg *sync.WaitGroup) {
+	defer wg.Done()
+	for k := 0; k <= 10000; k++ {
+		logger.Log(1, w)
+	}
 
-  fmt.Println(common.Sum(1,2))
-  
+}
+
+func main() {
+	logger, err := irlog.InitializeLogger("/home/irakli/go/src/Samples/Logs/log.txt", "/home/irakli/go/src/Samples/LogArchive/", 2, 1000)
+	if err != nil {
+		fmt.Println("errror")
+	}
+	wg := sync.WaitGroup{}
+	wg.Add(1)
+	go lg(logger, "1", &wg)
+	time.Sleep(2 * time.Second)
+	wg.Add(1)
+	go lg(logger, "2", &wg)
+	wg.Wait()
+	// go lg(logger, "2")
 }
